28a420a5e5fe4eac2b555b49952d709f1f14f860
lordxer0 <megaxero00@gmail.com>
2019-04-06 00:00:08 -0500

    ya falta poco para los crud base

---
+++ resources/views/Tipo_usuarios/ver.blade.php
@@ -0,0 +1,28 @@
+@extends('layout/plantilla')
+@section('content')
+    <div>
+        <a href="{{ url('tipo_documentos')}}" class="btn btn-info pull-right"> << Atrás </a>
+        <h1>Ver tipo_documentos</h1>
+    </div>
+    <hr>
+    <form class="form-horizontal">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="form-group">
+                    <label for="id" class="col-sm-2 control-label">Codigo Documento</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="id" placeholder="{{$tipo_documentos->tdoc_codigo}}" readonly>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="titulo" class="col-sm-2 control-label">Nombre documento</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="title" placeholder="{{$tipo_documentos->tdoc_nombre}}" readonly>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <hr>
+    </form>
+@stop
